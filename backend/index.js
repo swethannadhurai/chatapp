bca7e19c8292b59c4bf4522e6569029a31264c2d
Swetha <swethaannadhurai@gmail.com>
2025-09-17 23:24:35 +0530

    deployed

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://magical-wisp-ce12ab.netlify.app",
     credentials: true,
 }))
 
